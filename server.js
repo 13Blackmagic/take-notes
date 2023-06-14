cacac89c64f783b22b703fbe6e57a2dafa9ff319
13Blackmagic <123032739+13Blackmagic@users.noreply.github.com>
2023-06-13 19:58:59 -0600

    deleted

--- server.js
+++ server.js
@@ -2,3 +2,8 @@ const express = require('express');
 const path = require('path');
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
+
+const port = 3001;
+
+const app = express();
+
