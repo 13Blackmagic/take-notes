58d3f2efd0731ba5d175debb32dec2ab99ab6fba
13Blackmagic <123032739+13Blackmagic@users.noreply.github.com>
2023-06-16 22:53:19 -0600

    adding

--- server.js
+++ server.js
@@ -2,15 +2,12 @@ const express = require('express');
 const path = require('path');
 const apiRoutes = require('./routes/api-Routes.js');
 const htmlRoutes = require('./routes/html-routes.js');
-const importNotes = require('./db/dbNotes.json');
+
 
 const PORT = 3001;
 
 const app = express();
 
-importNotes.forEach(note => {
-  note.id = importNotes.indexOf(note) + 1;
-});
 
 
 app.use(express.json());
@@ -21,5 +18,4 @@ app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
-
 );
