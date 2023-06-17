25351d086e8709031b5df3b729c73ad496dbf3fb
13Blackmagic <123032739+13Blackmagic@users.noreply.github.com>
2023-06-16 22:24:36 -0600

    added notes

--- server.js
+++ server.js
@@ -2,10 +2,15 @@ const express = require('express');
 const path = require('path');
 const apiRoutes = require('./routes/api-Routes.js');
 const htmlRoutes = require('./routes/html-routes.js');
+const importNotes = require('./db/dbNotes.json');
 
 const PORT = 3001;
 
 const app = express();
+
+importNotes.forEach(note => {
+  note.id = importNotes.indexOf(note) + 1;
+});
 
 
 app.use(express.json());
